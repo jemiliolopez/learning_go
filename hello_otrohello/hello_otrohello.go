28b8e37bd6507dc3631f8c773c52541a4e9fdcfc
samyunodos <samyunodos@gmail.com>
2016-11-15 08:29:07 +0100

    all

---
+++ hello_otrohello/hello_otrohello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/samyunodos/learning_go/otrohello_lib"
+//	"pkg/otrohello"
+)
+
+//Esto es un comentario
+
+func main(){
+	fmt.Println("Hello Mundo\n")
+	otrohello_lib.Cascada()
+}
+
---
+++ otrohello_lib/otrohello_lib.go
@@ -0,0 +1,10 @@
+package otrohello_lib
+
+import "fmt"
+
+//Esto es un comentario
+
+func Cascada(){
+	fmt.Println("Otro hello")
+}
+
